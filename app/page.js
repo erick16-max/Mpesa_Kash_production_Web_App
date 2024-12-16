51cf74d825aedcb6cf6caf78039c0769affe61c2
erick16-max <erickgege16@gmail.com>
2024-12-16 09:38:29 +0300

    responsive screens

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Box, Stack } from "@mui/material";
 import Image from "next/image";
 import CustomAppBar from "./components/header/CustomAppBar";
@@ -7,45 +7,32 @@ import CustomFloatingButton from "./components/general/CustomFloatingButton";
 import { Suspense, useEffect, useState } from "react";
 import PageLoader from "./components/general/PageLoader";
 
-
 export default function Home() {
-  const [loading, setLoading] = useState(true)
-  const [scroll, setScroll] = useState(false)
-
-
+  const [loading, setLoading] = useState(true);
+  const [scroll, setScroll] = useState(false);
 
   const changeNavBg = () => {
     window.scrollY >= 10 ? setScroll(true) : setScroll(false);
   };
 
   useEffect(() => {
-    setLoading(false)
-    window.addEventListener('scroll', changeNavBg);
+    setLoading(false);
+    window.addEventListener("scroll", changeNavBg);
     return () => {
-      window.removeEventListener('scroll', changeNavBg);
+      window.removeEventListener("scroll", changeNavBg);
     };
   }, []);
 
-
-  if(loading) return <PageLoader />
+  if (loading) return <PageLoader />;
   return (
     <Suspense fallback={<PageLoader />}>
-        <Box
-    display={'flex'}
-    width={'100vw'}
-    flexDirection={'column'}
-    
-   >
-    <CustomAppBar scroll={scroll}/>
-    <Stack
-      py={2}
-      mt={'140px'}
-      gap={2}
-    >
-      <HeroSection />
-    </Stack>
-    <CustomFloatingButton />
-   </Box>
+      <Box display={"flex"} width={"100vw"} flexDirection={"column"} height={'100vh'} sx={{overflowX: 'hidden'}}>
+        <CustomAppBar scroll={scroll} />
+        <Stack py={2} mt={"140px"} gap={2}>
+          <HeroSection />
+        </Stack>
+        <CustomFloatingButton />
+      </Box>
     </Suspense>
   );
 }
