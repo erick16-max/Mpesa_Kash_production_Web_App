a1d358eaf97dc136172d2bbfa8b4f697c2c9053d
erick16-max <erickgege16@gmail.com>
2024-12-22 15:56:13 +0300

    connect and authenticate with deriv

--- app/finishaccount/SubmitPassword.js
+++ app/finishaccount/SubmitPassword.js
@@ -14,6 +14,7 @@ import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import { createUser } from "@/firebase/Firebase";
 import { useRouter } from "next/navigation";
 import { doc, getDoc, setDoc } from "firebase/firestore"; 
+import { db, auth } from "@/firebase.config";
 
 
 
