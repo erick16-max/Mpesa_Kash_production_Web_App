86707d4cb572297b6f1d3089b78f161f95bf0644
erick16-max <erickgege16@gmail.com>
2024-12-22 16:11:02 +0300

    connect and authenticate with deriv

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,16 +1,19 @@
 "use client"
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import PageLoader from "../components/general/PageLoader";
 
-const Auth = () => {
+const page = () => {
   const router = useRouter();
 
   useEffect(() => {
     // Redirect the user to /finishaccount directly
     router.push("/finishaccount");
+ 
   }, [router]);
 
-  return null; // No UI is rendered, just redirecting.
+
+  return <PageLoader />; // No UI is rendered, just redirecting.
 };
 
-export default Auth;
+export default page;
