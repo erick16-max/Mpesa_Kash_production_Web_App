a17a2e938c30666b6dbb06d930054b3fb11bcefe
erick16-max <erickgege16@gmail.com>
2025-01-02 12:29:18 +0300

    authentication logic

--- app/finishaccount/page.js
+++ app/finishaccount/page.js
@@ -3,16 +3,31 @@ import { Alert, Box, useMediaQuery, Stack, useTheme, Typography } from '@mui/mat
 import PageLayout from '../layout/PageLayout'
 import Link from 'next/link'
 import FinishAccountCard from './FinishAccountCard'
+import ColorModeContext from '@/theme/ThemeContextProvider'
+import { useContext } from 'react'
+import CustomAppBar from '../components/header/CustomAppBar'
 
 export default function page() {
+  const { isTablet } = useContext(ColorModeContext);
   
 
    
   return (
   
-<PageLayout>
+ <Box
+      width={"100%"}
+      display={"flex"}
+      flexDirection={"column"}
+      maxWidth={"1700px"}
+      margin={"auto"}
+    >
+      <Box width={"100%"}>
+        <CustomAppBar  />
+      </Box>
+      <Stack mt={"100px"} py={2} px={isTablet ? 2 : 3}>
       <FinishAccountCard />
+      </Stack>
+    </Box>
         
-</PageLayout>
   )
 }
